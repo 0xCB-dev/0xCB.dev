9926f236eddbc56eb6ef282fbcb5d134b1760024
Conor Burns <mail@conor-burns.com>
2021-04-12 18:52:34 +0200

    gatsby deploy script
    Signed-off-by: Conor Burns <mail@conor-burns.com>

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteTitle: '0xCB - Tech, Keyboards and more', // <title>
   heading: 'Tech, Keyboards and more',
-  subHeading: 'TBD',
+  subHeading:
+    '0xCB makes custom-made Keyboards, that are fully customizable. Handmade, precisely engineered keyboards made with the help of 3D-Printing and assembled in Germany.',
 };
