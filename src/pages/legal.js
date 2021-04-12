fd4dc57c86f649693250c4669a1c014a0380fd26
Conor Burns <mail@conor-burns.com>
2021-04-13 00:12:21 +0200

    gatsby deploy script
    Signed-off-by: Conor Burns <mail@conor-burns.com>

--- src/pages/legal.js
+++ src/pages/legal.js
@@ -13,90 +13,90 @@ const IndexPage = () => (
 
       <div className="wrapper">
         <div className="inner">
-          <b>Angaben gemäß § 5 TMG:</b>
+          <b>Legal Notice (§ 5 TMG)</b>
           <p>
             Conor Burns
             <br />
             Oberforstbacher Str. 54
             <br />
             52076 Aachen
+            <br />
+            NRW, Germany
           </p>
-          <b>Kontakt:</b>
+          <b>Contact</b>
           <p>E-Mail: mail@0xcb.dev</p>
-          <b>Streitschlichtung</b>
+
+          <b>Responsible for content (§ 55 Abs. 2 RStV)</b>
           <p>
-            Die Europäische Kommission stellt eine Plattform zur
-            Online-Streitbeilegung (OS) bereit:
+            Conor Burns
             <br />
-            <a
-              class="nonblock"
-              rel="noreferrer"
-              href="https://ec.europa.eu/consumers/odr"
-              target="_blank"
-            >
-              https://ec.europa.eu/consumers/odr
-            </a>
-            .<br />
-            Unsere E-Mail-Adresse finden Sie oben im Impressum.
+            Oberforstbacher Str. 54
             <br />
-            Wir sind nicht bereit oder verpflichtet, an
-            Streitbeilegungsverfahren vor einer
+            52076 Aachen
             <br />
-            Verbraucherschlichtungsstelle teilzunehmen.
+            NRW, Germany
           </p>
-          <b>Haftung für Inhalte</b>
+          <b>Disclaimer</b>
           <p>
-            Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
-            auf diesen Seiten nach den
-            <br />
-            allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir
-            als Diensteanbieter jedoch nicht
-            <br />
-            verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
-            überwachen oder nach Umständen zu forschen, die auf eine
-            rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung
-            oder Sperrung der Nutzung von Informationen nach den allgemeinen
-            Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist
-            jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten
-            Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
-            Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+            The content of this site has been created with the greatest of care.
+            I cannot, however, guarantee that the information in it is accurate,
+            complete or up-to-date. As a service provider I am responsible under
+            Section 6(1) of the German Media Services Inter-State Agreement and
+            Section 8(1) of the German Teleservices Act for my own content on
+            this site. Service providers are not however obliged to monitor
+            third party information transmitted or stored on their site by them
+            or to look for circumstances which may suggest a violation of the
+            law. This does not affect my statutory obligations to remove or
+            block the use of information. My liability in such cases shall
+            however commence from the time I become aware of an actual
+            violation. On becoming aware of such violations I shall remove this
+            content immediately.
           </p>
-          <b>Haftung für Links</b>
+          <b>Liability for links</b>
           <p>
-            Unser Angebot enthält Links zu externen Websites Dritter, auf deren
-            Inhalte wir keinen Einfluss haben.
-            <br />
-            Deshalb können wir für diese fremden Inhalte auch keine Gewähr
-            übernehmen. Für die Inhalte der
-            <br />
-            verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber
-            der Seiten verantwortlich. Die verlinkten
-            <br />
-            Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche
-            Rechtsverstöße überprüft. Rechtswidrige
-            <br />
-            Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine
-            permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
-            ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.
-            Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links
-            umgehend entfernen.
+            This website contains links to external third-party websites, over
+            the content of which I have no control. I cannot, therefore, make
+            any guarantees regarding this third-party content. Responsibility
+            for the content of linked sites lies solely with the provider or
+            operator of the site concerned. All linked sites were checked for
+            possible violations of the law when they were linked to mine. At
+            that time I was not aware of any content which may violate the law.
+            However, I cannot be expected to monitor the content of linked sites
+            on an ongoing basis unless I have reason to suspect a violation of
+            the law. On becoming aware of such a violation I shall remove the
+            respective link immediately.
           </p>
-          <b>Urheberrecht</b>
+          <b>Copyright</b>
           <p>
-            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
-            diesen Seiten unterliegen dem deutschen Urheberrecht. Die
-            Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
-            Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
-            schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
-            Downloads und Kopien dieser Seite sind nur für den privaten, nicht
-            kommerziellen Gebrauch gestattet.
-            <br />
-            Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
-            wurden, werden die Urheberrechte Dritter beachtet. Insbesondere
-            werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie
-            trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten
-            wir um einen entsprechenden Hinweis. Bei Bekanntwerden von
-            Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+            The operator of this site makes every effort to respect the
+            copyrights of outside parties and to employ his own material and
+            material in the public domain. All content and material on this site
+            created by the site operator is governed by German copyright law.
+            Downloads and copies of this site may be made for private,
+            non-commercial use only. Reproduction, processing, distribution and
+            any form of exploitation beyond that permitted by copyright law
+            requires the written consent of the author or creator concerned.
+            Contributions by third parties are identified as such. Should anyone
+            become aware of a possible copyright infringement, I kindly request
+            that you inform me of such. Upon becoming aware of such a violation
+            I shall remove the respective content immediately.
+          </p>
+          <b>Data protection</b>
+          <p>
+            As a rule the use of this website is possible without providing any
+            personally relevant data. Any provision of personal data (e.g. name,
+            address or email addresses) occurs entirely on a voluntary basis.
+            These data will not be provided to any third parties without the
+            user's express approval. Be aware that the transmission of data via
+            the internet (e.g. communication by email) is subject to security
+            gaps. Complete protection of data from unauthorized access by third
+            parties is not possible. The use of contact details published under
+            the statutory requirement to provide acknowledgements by third
+            parties for the purpose of the transmission of unsolicited
+            advertising and informational material is expressly opposed. The
+            site operator expressly reserves the right to take legal action
+            against the unsolicited mailing of advertising information by way of
+            spamming or similar.
           </p>
         </div>
       </div>
