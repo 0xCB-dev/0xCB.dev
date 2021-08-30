aef632d8de000aa7e813d46909fdcb4f75283a3f
Jakob170201 <Jakobkriener@web.de>
2021-08-30 21:08:04 +0200

    more text from Travis added

--- src/pages/static/index.js
+++ src/pages/static/index.js
@@ -22,13 +22,16 @@ const IndexPage = () => (
           <img className="image fit" src={picbg} alt="" />
           <div className="content">
             <p>
-              Here it is! The 0xCB Static. Feel free to scroll through here and
-              learn a bit about its features and look at a few nice pics of it!{' '}
+              And here it is in all it's glory, the 0xCB Static! <br />
+              We encourage you to take a scroll and learn all about it's
+              features, or just spend a couple minutes soaking in it's raw
+              industrial beauty.
               <br />
               <br />
-              <br />
-              As always head over to our Discord to always get the latest news
-              and just to hang out with us:
+              Always feel free to come say hi on our Discord server. Whether
+              it's to get the latest news on our products, get some help with
+              your build, show off your fancy new Static, or just say hi and
+              come hang out for a bit, we'd love to see you!
               <br />
               <br />
               <span>
@@ -52,15 +55,18 @@ const IndexPage = () => (
               <span className="image fit">
                 <img src={pictop} alt="" />
               </span>
-              The keyboard will be available as a DIY kit. The kit will come
-              with the plate for the switches, the bottom plate, an acrylic
-              sheet to cover the electronics, the PCB, and all its electronic
-              components including one OLED screen and a rotary encoder. Also
-              included will be the 3D-printed feet and all the brass standoffs,
-              screws, and rubber feet. As an added bonus you will get a
-              3D-Printed diode bender so you can bend your diodes easy and
-              exact. You will need to supply your own switches, screw-in
-              stabilizers, and keycaps.
+              Our new Static keyboard will be sold as a DIY build kit, and we're
+              especially excited to announce that it will be available soon
+              exclusively through our friend David over at
+              <a href="https://candykeys.com"> CandyKeys</a>! Each kit includes
+              a Static PCB, switch and bottom plate, EC11 rotary encoder and
+              knob, 0.91" OLED screen, acrylic OLED/electronics cover, all of
+              the required through-hole components and mounting hardware, plus a
+              pair of 3D printed feet. We've even gone so far as to design and
+              3D print a perfectly sized diode bender to put in each kit, so
+              you'll end up with not only the easiest but the cleanest build you
+              thought possible! All you need to supply are your own switches,
+              screw-in stabilizers, and keycaps.
               <br />
               <br />
             </p>
@@ -74,13 +80,21 @@ const IndexPage = () => (
               <span className="image fit">
                 <img src={picside} alt="" />
               </span>
-              The Keyboard sits flush on the table or at a 6° angle if you
-              prefer it that way. It has a USB-C port on the back for you to
-              connect it to your computer. With the rotary controller, you can
-              control your volume and on the OLED screen, there will be info
-              about what layer you are on and if Caps or Numlock is on. We also
-              added a second Layer to the OLED that you can switch to which
-              shows BongoCat which is animated when you begin typing.
+              The Static has two available orientations, a 6° angle via our 3D
+              printed feet or flat against your desk if that's more your
+              flavour. For both future-proofing and convenience sake we've gone
+              with a left-mounted USB-C port, we think this is ideal for the
+              majority of people. Using our stock firmware the rotary encoder
+              will function as volume control, but this is totally programmable
+              within QMK/VIA itself so you may find it useful for things like
+              page scrolling or video/audio seeking. By taking a glance at your
+              OLED screen, you'll quickly be able to tell not only what layer
+              you're on but information like whether caps/num lock is enabled.
+              Not only that, but we've added a hidden animated second layer to
+              your OLED that features everyone's favourite percussion-loving
+              feline, BongoCat. He'll show up to play the song of his people on
+              his drum whenever you begin typing, and will beat it faster and
+              faster as your own wpm increase!
               <br />
               <br />
               <br />
@@ -95,9 +109,15 @@ const IndexPage = () => (
               <span className="image fit">
                 <img src={picplate} alt="" />
               </span>
-              The base kits for the Static will feature black or white FR4
-              plates. However you can get a stainless steel and aluminium extra
-              to replace them with a higher quality feeling metal.
+              All of our base kits will come with your choice of either black or
+              white FR4 switch and bottom plates, for that nice flexy feel and
+              thocky sound profile so many of you have come to know and love. If
+              you prefer a firmer typing experience or a more premium look,
+              we'll also have a seperate upgrade available consisting of an
+              additional lasercut stainless steel bottom and aluminum switch
+              plate. We think this combination in particular captures the
+              industrial vibe we were after and we're very proud of it, we hope
+              you love it as much as we do!
             </p>
           </div>
         </div>
@@ -106,12 +126,15 @@ const IndexPage = () => (
         <div className="inner">
           <div className="content">
             <p>
-              The keyboard fully supports VIA and QMK so you can configure the
-              whole keyboard to have multiple Layers, what macros you want and
-              what the Rotary Encoder and OLED Screen should do, and many more
-              QMK quirks.
+              As we've previously mentioned, the Static keyboard natively
+              supports both QMK and VIA. This makes it incredibly easy to
+              configure your keyboard to your exact needs; from how many layers
+              you have and what each does, to specific workflow macros, to the
+              rotary encoder/OLED screen behaviour! The possibilities are truly
+              endless and we've really only scratched the surface here, you're
+              just gonna have to get your hands on one to see what we mean!
               <br />
-              You have these five options for the bottom row:
+              Please see our layout and supported bottom row options below:
               <span className="image fit">
                 <img src={piclayout} alt="" />
               </span>
